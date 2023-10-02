a17022329b0bb42df2d7faa5dce5c862856f29de
LucasLaPrune <lucaslaprune14@gmail.com>
2023-10-02 14:35:47 +0200

    Modif bug

--- PAGE/MainWindow.xaml.cs
+++ PAGE/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace PAGE
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PagePrincipale.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
