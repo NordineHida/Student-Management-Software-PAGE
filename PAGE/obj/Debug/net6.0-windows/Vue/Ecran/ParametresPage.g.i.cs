f435fbc05bd9ce5a878f9646cb7776432cbd2be6
Nordine <nordine.hida@iut-dijon.u-bourgogne.fr>
2023-10-06 21:31:29 +0200

    Ajout du projet de testunitaire SQLite

--- PAGE/obj/Debug/net6.0-windows/Vue/Ecran/ParametresPage.g.i.cs
+++ PAGE/obj/Debug/net6.0-windows/Vue/Ecran/ParametresPage.g.i.cs
@@ -56,13 +56,13 @@ namespace PAGE.Vue.Ressources {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PAGE;component/vue/ecran/parametrespage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PAGE;V1.0.0.0;component/vue/ecran/parametrespage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Vue\Ecran\ParametresPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -72,7 +72,7 @@ namespace PAGE.Vue.Ressources {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
