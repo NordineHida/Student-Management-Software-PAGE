c8aa397902fa0a38466f3db7f4f77147d5150398
Lucas Prunier <lucaslaprune14@gmail.com>
2023-10-06 10:38:50 +0200

    nothing

--- PAGE/obj/Debug/net6.0-windows/App.g.cs
+++ PAGE/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace PAGE {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PAGE {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             PAGE.App app = new PAGE.App();
             app.InitializeComponent();
