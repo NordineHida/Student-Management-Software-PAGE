1ac93539cd1abbbd5fef0bfe700f93fae9af9d82
Lucas Prunier <lucaslaprune14@gmail.com>
2023-11-06 14:05:54 +0100

    Mise en place du fichier de test

---
+++ TestAPI/TestsAPI.cs
@@ -0,0 +1,27 @@
+using PAGE.Model;
+using System.Net;
+
+namespace TestAPI
+{
+    public class TestsAPI
+    {
+        [Fact]
+        public void TestAddEtu()
+        {
+
+        }
+
+        [Fact]
+        public void TestGetAllEtu()
+        {
+
+        }
+
+        [Fact]
+        public void TestAddSeveralEtu()
+        {
+
+        }
+
+    }
+}
