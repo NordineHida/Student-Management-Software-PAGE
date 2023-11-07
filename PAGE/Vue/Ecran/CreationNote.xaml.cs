a8fa5becbdcf136a77747fa7e9a059e5949545e0
Laszlo Duszynski <laszlo.duszynski@iut-dijon.u-bourgogne.fr>
2023-11-07 17:29:55 +0100

    [PAGE] Changement dans le XAML pour pouvoir ouvrir la fenêtre de création de note

--- PAGE/Vue/Ecran/CreationNote.xaml.cs
+++ PAGE/Vue/Ecran/CreationNote.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PAGE.Model;
+﻿using PAGE.APIEtudiant.Stockage;
+using PAGE.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,20 +21,50 @@ namespace PAGE.Vue.Ecran
     /// </summary>
     public partial class CreationNote : Window
     {
-        public CreationNote(PAGE.Model.Etudiant etu)
+        private Note note;
+        /// <summary>
+        /// Constructeur de fenêtre CreationNote
+        /// </summary>
+        /// <param name="note"></param>
+        /// <author>Laszlo</author>
+        public CreationNote(Note note)
         {
             InitializeComponent();
-            DataContext = etu;
+            DataContext = note;
+            this.note = note;
         }
 
+        /// <summary>
+        /// Méthode renvoyant la note crée quand on clique sur le bouton Créer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author>Laszlo</author>
         private void ClickCreer(object sender, RoutedEventArgs e)
         {
-            DialogResult = true;
+            //si on a choisi une catégorie
+            if (ComboBoxCategorie.SelectedItem != null)
+            { 
+                //récupère la catègorie de la combobox et met la propriété "Catégorie" de la note à sa valeur
+                ComboBoxItem categorieChoisie =(ComboBoxItem)ComboBoxCategorie.SelectedItem;
+                string categorieChoisieString = categorieChoisie.ToString();
+                string[] motsCatChoisie = categorieChoisieString.Split(": ");
+                note.Categorie=motsCatChoisie[1];
+                //on crée la note
+                EtuDAO.Instance.CreateNote(note);
+                this.Close();
+            }
+            //Si on n'a pas choisi de catégorie, un message s'affiche
+            else { MessageBox.Show("Veuillez choisir une catégorie"); }
         }
 
+        /// <summary>
+        /// Méthode fermant la fenêtre sans renvoyer la note quand on clique sur le bouton annuler
+        /// </summary>
+        /// <author>Laszlo</author>
         private void ClickAnnuler(object sender, RoutedEventArgs e)
         {
-            DialogResult = false;
+            this.Close();
         }
     }
 }
