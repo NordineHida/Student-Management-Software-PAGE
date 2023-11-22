3094d21bbce94a3879e747cafcfeac942fef86e3
Lucas Prunier <lucaslaprune14@gmail.com>
2023-11-22 10:51:32 +0100

    [Test Unitaire] Ajout d'un test pour la création de notes

---
+++ TestExelPAGE/TestNote.cs
@@ -0,0 +1,30 @@
+﻿using PAGE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPAGE
+{
+    public class TestNote
+    {
+        [Fact]
+        public void TestNoteConstructor()
+        {
+            CATEGORIE categorie = CATEGORIE.MEDICAL;
+            DateTime datePublication = DateTime.Now;
+            NATURE nature = NATURE.RDV;
+            string commentaire = "Ceci est un commentaire.";
+            int apogeeEtudiant = 123;
+
+            Note note = new Note(categorie, datePublication, nature, commentaire, apogeeEtudiant);
+
+            Assert.Equal(categorie, note.Categorie);
+            Assert.Equal(datePublication, note.DatePublication);
+            Assert.Equal(nature, note.Nature);
+            Assert.Equal(commentaire, note.Commentaire);
+            Assert.Equal(apogeeEtudiant, note.ApogeeEtudiant);
+        }
+    }
+}
