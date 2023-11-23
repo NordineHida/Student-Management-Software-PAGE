18ec51286be832bac8cbbf0120b72e7a00aa08a0
YamatoNief <yamato.nief@iut-dijon.u-bourgogne.fr>
2023-11-23 09:35:01 +0100

    [Annee] Création des classes Annee et Promotion

---
+++ PAGE/Model/Annee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAGE.Model
+{
+    public class Annee
+    {
+        private int anneeDebut;
+        private Promotion premiereAnnee;
+        private Promotion deuxiemeAnnee;
+        private Promotion troisiemeAnnee;
+
+        public int AnneeDebut { get { return anneeDebut; } set {  anneeDebut = value; } }
+        public Promotion PremiereAnnee { get {  return premiereAnnee; } set {  premiereAnnee = value; } }
+        public Promotion DeuxiemeAnnee { get { return deuxiemeAnnee; } set { deuxiemeAnnee = value; } }
+        public Promotion TroisiemeAnnee { get { return troisiemeAnnee; } set { troisiemeAnnee = value; } }
+
+        public Annee() 
+        {
+        }  
+    }
+}
---
+++ PAGE/Model/Promotion.cs
@@ -0,0 +1,24 @@
+﻿using PAGE.Model.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAGE.Model
+{
+    public class Promotion
+    {
+        private Etudiants etudiants;
+        private NOMPROMOTION nomPromotion;
+
+        public Etudiants Etudiants { get {  return etudiants; } set {  etudiants = value; } }
+        public NOMPROMOTION NomPromotion { get { return nomPromotion; } set {  nomPromotion = value; } }
+
+        public Promotion(Etudiants etu, NOMPROMOTION nomPromo) 
+        {
+            this.etudiants = etu;
+            this.nomPromotion = nomPromo;
+        }
+    }
+}
