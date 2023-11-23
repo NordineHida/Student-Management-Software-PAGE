c12658a938a5984a18e83a00981fe424980cbb89
YamatoNief <yamato.nief@iut-dijon.u-bourgogne.fr>
2023-11-23 14:09:09 +0100

    [Annee] Implémentation coté client du DAO

---
+++ PAGE/Stockage/AnneeDAO.cs
@@ -0,0 +1,46 @@
+﻿using PAGE.Model;
+using PAGE.Vue.Ecran;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PAGE.Stockage
+{
+    public class AnneeDAO : IAnneeDAO
+    {
+        /// <summary>
+        /// Ajoute une Année à la BDD
+        /// </summary>
+        /// <param name="annee">Année à ajouter</param>
+        /// <returns>vrai si l'ajout est effectué</returns>
+        /// <author>Yamato</author>
+        public async Task CreateAnnee(Annee annee)
+        {
+            // Créez une instance de HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                // Spécifiez l'URL de l'API
+                string apiUrl = "https://localhost:7038/Annee/CreateAnnee";
+
+                // Convertissez l'Utilisateur en JSON
+                string anneeSerialise = JsonSerializer.Serialize(annee);
+
+                // Créez le contenu de la requête POST
+                HttpContent content = new StringContent(anneeSerialise, Encoding.UTF8, "application/json");
+
+                // Effectuez la requête POST
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    PopUp popUp = new PopUp("Année", "L'année est créé", TYPEICON.SUCCES);
+                    popUp.ShowDialog();
+                }
+            }
+        }
+    }
+}
---
+++ PAGE/Stockage/IAnneeDAO.cs
@@ -0,0 +1,24 @@
+﻿using PAGE.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAGE.Stockage
+{
+    /// <summary>
+    /// Interface de DAO pour les années entre le cient et l'API
+    /// </summary>
+    /// <author>Yamato</author>
+    interface IAnneeDAO
+    {
+        /// <summary>
+        /// Ajoute une Année à la BDD
+        /// </summary>
+        /// <param name="annee">Année à ajouter</param>
+        /// <returns>true si l'ajout est effectué</returns>
+        /// <author>Yamato</author>
+        public Task CreateAnnee(Annee annee);
+    }
+}
