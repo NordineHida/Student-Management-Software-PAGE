711854f618aa365d3d1e8e707f04182b872f2d2f
NordineHida <nordine.hida@iut-dijon.u-bourgogne.fr>
2023-11-24 10:41:06 +0100

    [EtudiantEtNoteComponent] - Creation du component pour les etudiants avec le nombre de note

---
+++ PAGE/Vue/Ecran/EtudiantEtNoteComponent.xaml.cs
@@ -0,0 +1,62 @@
+﻿using PAGE.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace PAGE.Vue.Ecran
+{
+    /// <summary>
+    /// Logique d'interaction pour EtudiantEtNoteComponent.xaml
+    /// </summary>
+    public partial class EtudiantEtNoteComponent : UserControl
+    {
+        /// <summary>
+        /// etudiant attaché au component
+        /// </summary>
+        /// <author>Nordine</author>
+        public Etudiant Etudiant { get; set; }
+
+        /// <summary>
+        /// Numero apogee de l'étudiant
+        /// </summary>
+        /// <author>Nordine</author>
+        public int NumeroApogee { get; set; }
+
+        /// <summary>
+        /// nom de l'étudiant
+        /// </summary>
+        /// <author>Nordine</author>
+        public string Nom { get; set; }
+
+        /// <summary>
+        /// prenom de l'étudiant
+        /// </summary>
+        /// <author>Nordine</author>
+        public string Prenom { get; set; }
+
+        /// <summary>
+        /// nombre de note de l'étudiant
+        /// </summary>
+        /// <author>Nordine</author>
+        public int NbNote { get; set; }
+
+        /// <summary>
+        /// Construit un component pour afficher l'étudiant en parametre
+        /// </summary>
+        /// <param name="etu">etudiant qu'on affiche</param>
+        /// <author>Nordine</author>
+        public EtudiantEtNoteComponent(Tuple<Etudiant,int> CoupleEtuNote)
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            this.Etudiant = CoupleEtuNote.Item1;
+            this.NumeroApogee = Etudiant.NumApogee;
+            //NOM Prenom
+            this.Nom = Etudiant.Nom.ToUpper();
+            this.Prenom = char.ToUpper(Etudiant.Prenom[0]) + Etudiant.Prenom.Substring(1).ToLower();
+            this.NbNote = CoupleEtuNote.Item2;
+
+        }
+    }
+}
