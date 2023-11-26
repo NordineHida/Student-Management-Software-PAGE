1a6a1044dc83ef0c3135132600942dbe5ffa96c5
Nordine <nordine.hida@iut-dijon.u-bourgogne.fr>
2023-11-26 11:20:06 +0100

    [AjoutAnnee] Correction IHM+ verification si l'année saisi est correct

--- PAGE/Vue/Ecran/AjoutAnnee.xaml.cs
+++ PAGE/Vue/Ecran/AjoutAnnee.xaml.cs
@@ -1,7 +1,10 @@
-﻿using PAGE.Model.Enumerations;
+﻿using PAGE.Model;
+using PAGE.Model.Enumerations;
 using PAGE.Model.PatternObserveur;
+using PAGE.Stockage;
 using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace PAGE.Vue.Ecran
 {
@@ -16,12 +19,62 @@ namespace PAGE.Vue.Ecran
         /// <author>Nordine/Yamato</author>
         public int AnneeSaisie
         {
-            get { return Int32.Parse(txtAnneeSaisie.Text); }
+            get { return getAnneeSaisie(); }
         }
         public AjoutAnnee()
         {
             InitializeComponent();
+            txtAnneeSaisie.PreviewTextInput += TextBox_PreviewTextInput;
         }
+
+        /// <summary>
+        /// Verifie qu'on saisi seulement des chiffres
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <author>Nordine</author>
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // On verifie si c'est un nombre
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                // Ignore le caractère si ce n'est pas un chiffre
+                e.Handled = true; 
+            }
+        }
+
+        /// <summary>
+        /// renvoi l'année saisi si elle est correct (entre 1800 et mtn+ans)
+        /// </summary>
+        /// <returns>année saisi</returns>
+        /// <author>Nordine</author>
+        private int getAnneeSaisie()
+        {
+            int anneeSaisie=-1;
+
+            if (Int32.TryParse(txtAnneeSaisie.Text, out anneeSaisie))
+            {
+                int anneeMin = 1800;
+                int anneeMax = DateTime.Now.Year + 2;
+
+                if (anneeSaisie < anneeMin || anneeSaisie > anneeMax)
+                {
+                    anneeSaisie = -1;
+
+                    // Afficher la pop-up d'erreur
+                    string titre = (Parametre.Instance.Langue == LANGUE.FRANCAIS) ? "Erreur année" : "Bad year";
+                    string message = (Parametre.Instance.Langue == LANGUE.FRANCAIS) ? "Veuillez sélectionner une année correcte" : "Please select a correct year";
+
+                    PopUp popUp = new PopUp(titre, message, TYPEICON.ERREUR);
+                    popUp.ShowDialog();
+                }
+                else
+                    anneeSaisie = Int32.Parse(txtAnneeSaisie.Text);
+            }
+
+            return -1; 
+        }
+
 
         /// <summary>
         /// Ferme la fenêtre lors du click sur le bouton OK
@@ -29,17 +82,24 @@ namespace PAGE.Vue.Ecran
         /// <author>Yamato</author>
         private void OkClick(object sender, RoutedEventArgs e)
         {
-            if (AnneeSaisie != null)
-            {
+
+    
 
                 this.Close();
-            }
+           
             else
             {
-                PopUp popUp = new PopUp("Erreur année", "Veuillez selectionner une année", TYPEICON.ERREUR);
-                popUp.ShowDialog();
+                if (Parametre.Instance.Langue == LANGUE.FRANCAIS)
+                {
+                    PopUp popUp = new PopUp("Erreur année", "Veuillez selectionner une année correcte", TYPEICON.ERREUR);
+                    popUp.ShowDialog();
+                }
+                else
+                {
+                    PopUp popUp = new PopUp("Bad year", "Please select a correct year", TYPEICON.ERREUR);
+                    popUp.ShowDialog();
+                }
             }
         }
-
     }
 }
